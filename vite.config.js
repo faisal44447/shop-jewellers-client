da90be4fe13005e72e6df406df3bbd212d4a62b9
Faisal <your-email@example.com>
2026-05-18 23:11:50 +0600

    added an

--- vite.config.js
+++ vite.config.js
@@ -4,12 +4,13 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/', // 👈 এই লাইনটি যুক্ত করা হয়েছে, যা রাউটিংয়ের মিম টাইপ এরর দূর করবে
   optimizeDeps: {
     include: ['react-helmet-async']
   },
   resolve: {
-  alias: {
-    events: "events"
-  }
-},
-})
+    alias: {
+      events: "events"
+    }
+  },
+});
